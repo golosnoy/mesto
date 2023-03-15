d729f64abd015acb46d3ed983842a3077e40ccfb
Sergey Golosnoy <sergeygolosnoy@gmail.com>
2023-03-15 19:51:38 +0300

    ПР4 Версия 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,30 @@
-/* 
-Уважаемый ревьюер!
-Спасибо за комментарии!
-Если у Вас будут замечания касательно этого кода, большая просьба подробно 
-написать, что необходимо исправить. 
-Функционал привел в соответствие с заданием.
-Нужно ли переписывать код по шаблону из задания?
-*/
-
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 let profileEditButton = document.querySelector(".profile__edit-button");
-let popupTitle = document.querySelector(".popup__input-title");
-let popupSubtitle = document.querySelector(".popup__input-subtitle");
+let popupTitle = document.querySelector('input[name="author_name"]');
+let popupSubtitle = document.querySelector('input[name="author_about"]');
 let popupSubmitButton = document.querySelector(".popup__submit-button");
 let popupCloseButton = document.querySelector(".popup__close-button");
 let popup = document.querySelector(".popup");
 let popupContainer = document.querySelector(".popup__container");
 
-function EditProfile() {
+function editProfile() {
     popupTitle.value = profileTitle.textContent;
     popupSubtitle.value = profileSubtitle.textContent;
     popup.classList.add('popup_opened');
-    popupContainer.classList.add('popup_opened');
 }
 
-function ClosePopup() {
+function closePopup() {
     popup.classList.remove('popup_opened');
-    popupContainer.classList.remove('popup_opened');
 }
 
-function SubmitProfile(evt) {
+function submitProfile(evt) {
     evt.preventDefault();
     profileTitle.textContent = popupTitle.value;
     profileSubtitle.textContent = popupSubtitle.value;
-    ClosePopup();
+    closePopup();
 }
 
-profileEditButton.addEventListener("click", EditProfile);
-popupCloseButton.addEventListener("click", ClosePopup);
-popupSubmitButton.addEventListener("click", SubmitProfile);
+profileEditButton.addEventListener("click", editProfile);
+popupCloseButton.addEventListener("click", closePopup);
+popupContainer.addEventListener("submit", submitProfile);
