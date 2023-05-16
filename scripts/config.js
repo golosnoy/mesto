d2774d47500c13c0fc6e5ec193e99a513cb803d6
Sergey Golosnoy <sergeygolosnoy@gmail.com>
2023-05-16 21:01:41 +0300

    ПР8 Проверка 1

--- scripts/config.js
+++
@@ -1,9 +0,0 @@
-export const validationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-button',
-  inputErrorSelector: '.popup__input-error',
-  inactiveButtonClass: 'popup__submit-button_inactive',
-  inputErrorClass: 'popup__input_type_error',
-  spanErrorClass: 'popup__input-error_active'
-}
